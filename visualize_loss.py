20184e7bd02d5fd5edacb3dfb5fec97c1b114aeb
ufownl <ufownl@gmail.com>
2019-01-19 15:10:38 +0800

    Adjust the script of loss visualization

--- visualize_loss.py
+++ visualize_loss.py
@@ -2,23 +2,28 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-training_loss = []
-validating_loss = []
 
-regex = re.compile(".* training_loss (\S+).* validating_loss (\S+)")
+def visualize(lines):
+    regex = re.compile(".* training_loss (\S+).* validating_loss (\S+)")
+    training_loss = []
+    validating_loss = []
+    for line in lines:
+        m = regex.match(line)
+        if m:
+            training_loss.append(float(m.group(1)))
+            validating_loss.append(float(m.group(2)))
+    plt.plot(np.array(training_loss), label="training loss")
+    plt.plot(np.array(validating_loss), label="validating loss")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    m = regex.match(line)
-    if m:
-        training_loss.append(float(m.group(1)))
-        validating_loss.append(float(m.group(2)))
 
-plt.plot(np.array(training_loss), label="training loss")
-plt.plot(np.array(validating_loss), label="validating loss")
-plt.grid(True)
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    lines = []
+    while True:
+        try:
+            lines.append(input())
+        except EOFError:
+            break
+    visualize(lines)
